f77d49dd0ecbdda02d79df30a051188daed949e9
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-10 16:54:45 -0500

    Users can add comments to tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,6 +11,7 @@ class TicketsController < ApplicationController
   end
 
   def create
+    binding.pry
     @ticket = @project.tickets.build(ticket_params)
     if @ticket.save
       flash[:notice] = "Ticket has been created."
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910170857) do
+ActiveRecord::Schema.define(version: 20130910200108) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20130910170857) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "tags_tickets", id: false, force: true do |t|
+    t.integer "tag_id"
+    t.integer "ticket_id"
   end
 
   create_table "tickets", force: true do |t|
