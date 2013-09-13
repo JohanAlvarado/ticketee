f64203608b68adf28c4e49fba05665a09684f7cc
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-13 12:49:34 -0500

    Add devise for token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  devise :token_authenticatable
   has_secure_password
   after_save :send_notification
 
   def send_notification
     Notifier.user_created(self).deliver
   end
-
+  
 end
