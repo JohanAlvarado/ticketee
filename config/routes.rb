6be5d049f5bf57aa9b05c88e82b5dcf8faa71ecb
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-13 10:20:39 -0500

    Add resources for api

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,18 @@ Ticketee::Application.routes.draw do
     end
   end
 
+  Ticketee::Application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :projects
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
+
 end
