41b163079685b81278e295b65ce3f9eec2d641e6
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-19 11:28:26 -0500

    Extend class User with Module OmniauthCallbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  extend OmniauthCallbacks
   has_secure_password
   has_many :projects
+  devise :token_authenticatable, :omniauthable
+  before_save :ensure_authentication_token
+
 end
