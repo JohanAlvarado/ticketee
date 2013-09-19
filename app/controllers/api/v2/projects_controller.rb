ae02eb0ceb3447e92d0e7384fb9fd2df2150cd4d
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-19 15:21:28 -0500

    Create Api V2 for pagination

---
+++ app/controllers/api/v2/projects_controller.rb
@@ -0,0 +1,40 @@
+class Api::V2::ProjectsController < Api::BaseController
+  before_filter :find_project, :only => [:show,:update,:destroy]
+
+  def index
+    respond_with(current_user.projects)
+  end
+
+  def show
+    respond_with(@project, :methods => "last_ticket")
+  end
+
+  def create
+    project = Project.new(params[:project])
+    if project.save
+      respond_with(project, :location => api_v1_project_path(project))
+    else
+      respond_with(project)
+    end
+  end
+
+  def update
+    @project.update_attributes(params[:project])
+    respond_with(@project)
+  end
+
+  def destroy
+    @project.destroy
+    respond_with(@project)
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    error = { :error => "The project you were looking for " +
+              "could not be found."}
+    respond_with(error, :status => 404)
+  end
+
+end
