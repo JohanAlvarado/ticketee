7db5d1a801d8080de6922a2f6bfe78cc63045ab9
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-19 11:22:11 -0500

    Add strategy for login with github

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,8 +10,9 @@ module Users
 
     def github
       @user = User.find_or_create_for_github(env["omniauth.auth"])
-      flash[:notice] = "Signed in with GitHub successfully."
-      sign_in_and_redirect @user, :event => :authentication
+      session[:user_id] = @user.id
+      flash[:notice] = "Signed in successfully."
+      redirect_to root_url
     end
 
   end
