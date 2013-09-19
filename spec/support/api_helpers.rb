2a928b5a92f5f5c3014d3d1ea87b92363e75fadc
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-19 13:22:27 -0500

    Delete files

--- spec/support/api_helpers.rb
+++
@@ -1,9 +0,0 @@
-module ApiHelper
-  include Rack::Test::Methods 
-  def app <co id="ch13_458_2"/>
-    Rails.application
-  end
-end
-RSpec.configure do |c|
-  c.include ApiHelper, :type => :api
-end
