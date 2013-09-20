51829c3663ab0cdfb69bc440017f4a56ca82b78a
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-20 11:39:02 -0500

    Add require action_caching

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,8 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
+require 'actionpack/action_caching'
 Bundler.require(:default, Rails.env)
-
+Bundler.require(*Rails.groups(assets: %w(development test)))
 module Ticketee
   class Application < Rails::Application
   end
