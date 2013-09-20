d8989b3e75de7a577857ae2695632884b3ef407d
JohanAlvarado <johan.alvaradohdz@gmail.com>
2013-09-20 11:40:57 -0500

    Add seed for tickets and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 State.create(:name=> "New",:background => "#85FF00",:color=> "white")
 State.create(:name=> "Open",:background => "#00CFFD",:color=> "white")
 State.create(:name=> "Closed",:background => "black",:color=> "white")
+project = Project.first
+100.times do |i|
+  project.tickets.create!(
+    :title => "Fake ticket",
+    :description => "Fake description",
+    :user => User.first
+  )
+end
